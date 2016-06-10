61db70a9f5ffcfdcd7c4a846f0cd00a22788616c
David Tran <djtran@bu.edu>
2016-06-10 14:52:53 -0400

    Made many get and post requests

--- BadgeClient/src/main/java/org/cidarlab/badgeclient/PingAWS.java
+++ BadgeClient/src/main/java/org/cidarlab/badgeclient/PingAWS.java
@@ -5,62 +5,149 @@
  */
 package org.cidarlab.badgeclient;
 
-
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import java.awt.BorderLayout;
+import java.awt.Image;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  *
  * @author david
  */
 public class PingAWS {
-    
+
     public static void main(String[] args) throws IOException, MalformedURLException, UnirestException {
+//        //LOGIN AS ADMIN PLS
         String targetURL = "http://52.39.236.237:8080/";
-        String ping = loginAdmin(targetURL);
-        
-        String[] params = new String[6];
-        params[0] = "Test Badge";
-        params[1] = "Testing post request for peasantry badge creation";
-        params[2] = "Bomberman.png";
-        params[3] = "Being the best peasant you can be";
-        params[4] = "Tag you're it";
-        params[5] = "ME";
-        
-        String badgePls = createBadge(targetURL, params);
-        System.out.println(badgePls);
+        boolean auth = login(targetURL, "beepboop@gmail.com", "hello");
+
+        if (auth) {
+            //BADGE CREATE
+            String[] params = new String[6];
+            params[0] = "Test Badge";
+            params[1] = "Testing post request for peasantry badge creation";
+            params[2] = "Bomberman.png";
+            params[3] = "Being the best peasant you can be";
+            params[4] = "Tag you're it";
+            params[5] = "ME";
+
+            System.out.println(auth);
+
+            //FILE GET
+            InputStream sonic = getFile(targetURL, "images/Sonic.png");
+            Image image = ImageIO.read(sonic);
+
+            JFrame frame = new JFrame("Sonic");
+            JLabel label = new JLabel(new ImageIcon(image));
+            frame.getContentPane().add(label, BorderLayout.CENTER);
+            frame.pack();
+            frame.setVisible(true);
+        }
     }
 
-    public static String loginAdmin(String targetURL) throws MalformedURLException, IOException, UnirestException {
+    public static boolean login(String targetURL, String email, String password) throws MalformedURLException, IOException, UnirestException {
         HttpResponse<String> response = Unirest.post(targetURL + "login")
-                .field("email","beepboop@gmail.com")
-                .field("password","hello")
+                .field("email", email)
+                .field("password", password)
                 .asString();
-        
-        HttpResponse<String> myProfile = Unirest.get(targetURL + "myprofile")
+
+        String rp = response.getBody();
+        Document page = Jsoup.parse(rp);
+
+        Elements hello = page.getElementsByTag("h2");
+        for (Element msg : hello) {
+            String parse = msg.text();
+            if (parse.contains("Welcome back")) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static InputStream getFile(String baseURL, String extension) throws UnirestException {
+        HttpResponse<InputStream> response = Unirest.get(baseURL + extension)
+                .header("content-type", "*/*")
+                .asBinary();
+
+        return response.getBody();
+    }
+
+    public static boolean register(String URL, String name, String email, String password) throws UnirestException {
+        if (name.isEmpty() || password.isEmpty()) {
+            return false;
+        }
+        HttpResponse<String> response = Unirest.post(URL + "register")
+                .field("name", name)
+                .field("email", email)
+                .field("password", password)
+                .field("passwordcheck", password)
                 .asString();
-        
-        String rpString = myProfile.getBody().toString();
-        return rpString;
-        
+
+        Document doc = Jsoup.parse(response.getBody());
+        Elements ele = doc.getElementsByTag("h2");
+        for (Element wap : ele) {
+
+            if (wap.toString().contains(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean createBadge(String targetURL, String name, String description, String image, String criteria, String tags, String issuer) throws UnirestException {
+        HttpResponse<String> response = Unirest.post(targetURL + "admin-badge")
+                .field("name", name)
+                .field("description", description)
+                .field("image", image)
+                .field("criteria", criteria)
+                .field("tags", tags)
+                .field("issuer", issuer)
+                .asString();
+
+        Document doc = Jsoup.parse(response.getBody());
+        Elements ele = doc.getElementsByTag("h2");
+
+        for (Element hehe : ele) {
+            if (hehe.toString().contains("Your badge was successfully created!")) {
+                return true;
+            }
+        }
+        return false;
     }
     
-    public static String createBadge(String targetURL, String[] params) throws UnirestException     
+    public static boolean createIssuer(String targetURL, String name, String description, String issuerURL) throws UnirestException
     {
-        HttpResponse<String> response = Unirest.post(targetURL+"admin-badge")
-                .field("name", params[0])
-                .field("description", params[1])
-                .field("image", params[2])
-                .field("criteria", params[3])
-                .field("tags", params[4])
-                .field("issuer", params[5])
+        HttpResponse<String> response = Unirest.post(targetURL + "issuer")
+                .field("name", name)
+                .field("description", description)
+                .field("url", issuerURL)
                 .asString();
         
-        return response.getBody().toString();
+        Document doc = Jsoup.parse(response.getBody());
+        Elements all = doc.getElementsByTag("p");
         
+        for(Element disOne : all)
+        {
+            if(disOne.toString().contains("Your issuer has been created!"))
+            {
+                return true;
+            }
+        }
+        
+        return false;
     }
 
 }
