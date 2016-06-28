178b53d472c30a88b50828188965d62b40ce6bf7
jkozol <jacob_kozol@yahoo.com>
2016-06-28 17:03:54 -0400

    Initial Commit

---
+++ Spring_Framework_Jetty/src/main/java/hello/Args.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author jacob
+ */
+public class Args {
+    
+//    public static final String clothoLocation = "wss://clothocad.org:8443/websocket";
+//    public static final String phagebookBaseURL = "cidarlab.org:9090";
+
+    public static final String clothoLocation = "wss://localhost:8443/websocket";
+    public static final String phagebookBaseURL = "localhost:9090";
+
+
+    public static final String defaultPhagebookUsername = "phagebook";
+    public static final String defaultPhagebookPassword = "backend";
+    
+    public static String getFilepath()
+    {
+        String filepath = Args.class.getClassLoader().getResource(".").getPath();
+        filepath = filepath.substring(0,filepath.lastIndexOf("/target/"));
+        filepath += "/";
+        return filepath;
+    }
+    
+    public static String getResourcesFilepath(){
+        return getFilepath() + "resources/";
+    }
+}
